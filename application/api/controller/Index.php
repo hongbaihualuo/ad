4a5d230af3a49d6b8b1728a5063e8c79011d0189
lolicc <1315381101@qq.com>
2019-03-04 17:47:53 +0800

    js修改测试

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -1,25 +1,42 @@
 <?php
-namespace app\api\contronller;
+namespace app\api\controller;
 
 
 use app\api\model\Ad;
 use app\api\model\AdType;
+use think\Controller;
 
-class Index{
+class Index extends Controller{
     public function get_ad()
     {
         header("Access-Control-Allow-Origin: *");
         header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With');
 
         $type_id = input('id');
+        $result = ['code'=>0,'msg'=>'','data'=>''];
+
+        if (!is_numeric($type_id)) {
+            $result['code'] = 1;
+            $result['msg'] = 'ID错误';
+            die(json_encode($result));
+        }
 
         $ad = new Ad();
         $adType = new AdType();
 
         $dataType = $adType->get($type_id);
         $datalist = $ad->get_ad("ad_type = {$type_id}",1000);
+        if ($dataType) {
+            $result['code'] = 1;
+            $result['msg'] = '未找到该广告';
+            die(json_encode($result));
+        }
 
-        $result = ['code'=>0,'msg'=>'','data'=>''];
+        if (count($datalist) <= 0) {
+            $result['code'] = 1;
+            $result['msg'] = '没有设置广告';
+            die(json_encode($result));
+        }
 
         $rand_keys = array_rand($datalist, 1);
         $dataAd = $datalist[$rand_keys[0]];
